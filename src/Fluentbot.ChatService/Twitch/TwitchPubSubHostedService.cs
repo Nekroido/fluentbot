5c40ea9bcbbfcc0125c589bf3c58046861a93cf6
Artem Smirnov <artem.smirnov@mollie.com>
2021-01-04 16:48:28 +0100

    Removed hard-coded pub/sub topic subscription

--- src/Fluentbot.ChatService/Twitch/TwitchPubSubHostedService.cs
+++ src/Fluentbot.ChatService/Twitch/TwitchPubSubHostedService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -5,27 +6,54 @@ using Fluentbot.Contracts.Messages.Stream;
 using MassTransit;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using TwitchLib.PubSub;
 
 namespace Fluentbot.ChatService.Twitch
 {
     public class TwitchPubSubHostedService : IHostedService
     {
-        private readonly ILogger<TwitchPubSubHostedService> _logger;
         private readonly IBus _bus;
+        private readonly TwitchPubSub _client;
+        private readonly IOptionsMonitor<TwitchSection> _config;
+        private readonly ILogger<TwitchPubSubHostedService> _logger;
         private readonly IMapper _mapper;
-        private TwitchPubSub _client;
 
-        public TwitchPubSubHostedService(ILogger<TwitchPubSubHostedService> logger, IBus bus, IMapper mapper)
+        public TwitchPubSubHostedService(
+            IOptionsMonitor<TwitchSection> config,
+            ILogger<TwitchPubSubHostedService> logger,
+            IBus bus,
+            IMapper mapper
+        )
         {
-            _logger = logger;
-            _bus = bus;
-            _mapper = mapper;
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _bus = bus ?? throw new ArgumentNullException(nameof(bus));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _client = new TwitchPubSub();
 
-            _client.ListenToVideoPlayback("joystickow");
-
+            SubscribeToTopics();
             RegisterListeners();
+
+            _config.OnChange(s => SubscribeToTopics());
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _client.Connect();
+            _logger.LogInformation("Connected to Twitch Pub/Sub");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _client.Disconnect();
+            _logger.LogInformation("Disconnected from Twitch Pub/Sub");
+        }
+
+        private void SubscribeToTopics()
+        {
+            foreach (var channel in _config.CurrentValue.EnabledChannels)
+                _client.ListenToVideoPlayback(channel);
         }
 
         private void RegisterListeners()
@@ -41,18 +69,6 @@ namespace Fluentbot.ChatService.Twitch
             _client.OnStreamDown += async (s, e) => await _bus.Publish(_mapper.Map<StreamEnded>(e));
 
             _client.OnRaidGo += async (s, e) => await _bus.Publish(_mapper.Map<RaidReceived>(e));
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            _client.Connect();
-            _logger.LogInformation("Connected to Twitch Pub/Sub");
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            _client.Disconnect();
-            _logger.LogInformation("Disconnected from Twitch Pub/Sub");
         }
     }
 }
